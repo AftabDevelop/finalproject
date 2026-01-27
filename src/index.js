8ab9605c91b435fb1dd8cdd9c6c931d9a2b2f0ae
AftabDevelop07 <mohdaftabalamsayyed@gmail.com>
2026-01-27 19:32:13 +0530

    modify index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const cookieParser = require("cookie-parser"); // ✅ import as cookieParser
 const authRouter = require("./routes/userAuth");
 const problemrouter = require("./routes/problemcreator");
 const submitRouter = require("./routes/submit");
-const aiRouter = require("./routes/aiChatting");
 const cors = require('cors');
 
 app.use(cors({
@@ -23,7 +22,7 @@ app.use(cookieParser()); // ✅ use cookieParser()
 app.use("/user", authRouter);
 app.use("/problem", problemrouter);
 app.use("/submission", submitRouter);
-app.use("/ai",aiRouter);
+
 
 const Initalizeconnection = async () => {
   try {
--- src/routes/aiChatting.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const aiRouter = express.Router();
-const userMiddleware = require("../middleware/usermiddel");
-const solveDoubt = require("../controllers/solveDoubts");
-
-aiRouter.post('/chat', userMiddleware, solveDoubt);
-
-module.exports = aiRouter;
